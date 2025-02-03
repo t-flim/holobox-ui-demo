ddf62e034d7726a7450faa3d176118fa76683ae3
Tey Fu Lim <46584666+t-flim@users.noreply.github.com>
2025-02-03 16:46:13 +0800

    Another Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+const button = $(".btn");
+const optionButton = $(".option-btn");
+const buttonDiv = $(".buttons");
+const optionDiv = $(".options");
+
+button.on("click", function() {
+    buttonDiv.addClass("hidden");
+    optionDiv.removeClass("hidden");
+    
+    optionButton.on("click", function(e) {
+        const code = e.currentTarget.innerText;
+
+        switch (code) {
+            case "Home":
+                returnHome()
+                break;
+            
+            case "Option 1":
+                playVideo("1");
+                break;
+
+            case "Option 2":
+                playVideo("2");
+                break;
+
+            case "Option 3":
+                playVideo("3");
+
+            default:
+                console.log(code);
+                break;
+        }
+    })
+});
+
+function returnHome() {
+    setTimeout(function() {
+        optionDiv.addClass("hidden");
+        buttonDiv.removeClass("hidden");
+    }, 150);
+}
+
+function playVideo(num) {
+    optionDiv.addClass("hidden");
+    setTimeout(function() {
+        $("#video").attr("src", "video/sample video_" + num + ".mp4");
+        $("#video").removeClass("hidden");
+        $("#video")[0].play();
+    }, 150)
+
+    $("#video").on("ended", function() {
+        setTimeout(function() {
+            $("#video").addClass("hidden");
+            buttonDiv.removeClass("hidden");
+        }, 150);
+    })
+}
